ea479374d46c8246bf971c0314ba4da93bfad533
Phuong Nguyen <hnguyenphuong09@gmail.com>
2024-03-13 17:12:58 +0700

    Spring boot project initialization

---
+++ thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/ThitracnghiemApplication.java
@@ -0,0 +1,14 @@
+package com.laptrinhweb.thitracnghiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThitracnghiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThitracnghiemApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ thitracnghiem/src/test/java/com/laptrinhweb/thitracnghiem/ThitracnghiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptrinhweb.thitracnghiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThitracnghiemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
