f97f6826a10d1703eb456a301e7862605c449bcc
nhoczui3110 <tranvietquang3110@gmail.com>
2024-04-19 17:52:16 +0700

    Add common.css

--- thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Security/SecurityConfig.java
+++ thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Security/SecurityConfig.java
@@ -5,38 +5,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
     // @Autowired
     // private UserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/login","/javascript/**","/assets/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .formLogin((form) -> form
-                .loginPage("/student/home")
-                .permitAll()
-                .defaultSuccessUrl("/student/home", true)
-            )
-            .logout((logout) -> logout.permitAll());
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login", "/javascript/**", "/assets/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        // .loginPage("/student/home")
+                        .permitAll()
+                        .defaultSuccessUrl("/admin/subject", true))
+                .logout((logout) -> logout.permitAll());
         return http.build();
-    }    
-
-    @Bean 
-    public PasswordEncoder passwordEncoder() { 
-        return new BCryptPasswordEncoder(); 
     }
-    // @Bean
-	// public UserDetailsService userDetailsService() {
-	// 	return userDetailsService;
-	// }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
