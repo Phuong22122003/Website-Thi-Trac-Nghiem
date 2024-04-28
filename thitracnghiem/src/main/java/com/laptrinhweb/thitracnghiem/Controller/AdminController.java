81e5dc98bf836d77b6726ccf43a346193ec47e10
nhoczui3110 <tranvietquang3110@gmail.com>
2024-04-28 20:10:09 +0700

    Change process exception for subject page and add route for lecturer

--- thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Controller/AdminController.java
+++ thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,14 +15,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.laptrinhweb.thitracnghiem.Entity.GiangVien;
 import com.laptrinhweb.thitracnghiem.Entity.MonHoc;
+import com.laptrinhweb.thitracnghiem.Service.GiangVienService;
 import com.laptrinhweb.thitracnghiem.Service.MonHocService;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
     @Autowired
     MonHocService monHocService;
+    @Autowired
+    GiangVienService giangVienService;
 
     @GetMapping("/student")
     public String student() {
@@ -32,11 +41,6 @@ public class AdminController {
     @GetMapping("/class")
     public String lophoc() {
         return "/admin/class";
-    }
-
-    @GetMapping("/lecturer")
-    public String lecturer() {
-        return "/admin/lecturer";
     }
 
     @GetMapping("/subject")
@@ -58,24 +62,9 @@ public class AdminController {
         return new MonHoc();
     }
 
-    // @PostMapping("/addMonHoc")
-    // public String addMonHoc(@RequestParam(name = "mamh") String mamh,
-    // @RequestParam(name = "tenmh") String tenmh,
-    // @RequestParam(name = "soTietLt") int soTietLt, @RequestParam(name =
-    // "soTietTh") int soTietTh,
-    // RedirectAttributes redirectAttributes) {
-    // MonHoc monHoc = new MonHoc(mamh, tenmh, soTietLt, soTietTh, false);
-    // monHoc.setTrangThaiXoa(false);
-    // int statusThemMonHoc = monHocService.addMonHoc(monHoc);
-    // redirectAttributes.addFlashAttribute("statusThemMonHoc", statusThemMonHoc);
-    // return "redirect:/admin/subject";
-    // }
-
     @PostMapping("/addMonHoc")
-    public String addMonHoc(@ModelAttribute("monHoc") MonHoc monHoc,
+    public String addMonHoc(@Valid @ModelAttribute("monHoc") MonHoc monHoc,
             RedirectAttributes redirectAttributes, BindingResult errors) {
-        String errorMessage = "";
-
         if (monHoc.getMamh().trim().length() == 0) {
             errors.rejectValue("mamh", "monHoc", "Mã môn học không được bỏ trống");
         }
@@ -90,11 +79,10 @@ public class AdminController {
         }
 
         if (errors.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errorAdd", true);
             for (FieldError error : errors.getFieldErrors()) {
-                errorMessage = error.getDefaultMessage();
-                break; // Chỉ lấy lỗi đầu tiên
+                redirectAttributes.addFlashAttribute("errorAdd_" + error.getField(), error.getDefaultMessage());
             }
-            redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
             return "redirect:/admin/subject";
         }
 
@@ -103,18 +91,6 @@ public class AdminController {
         redirectAttributes.addFlashAttribute("statusThemMonHoc", statusThemMonHoc);
         return "redirect:/admin/subject";
     }
-
-    // @PostMapping("/editMonHoc")
-    // public String editMonHoc(@RequestParam("mamh") String mamh,
-    // @RequestParam("tenmh") String tenmh,
-    // @RequestParam("soTietLt") int soTietLt,
-    // @RequestParam("soTietTh") int soTietTh, RedirectAttributes
-    // redirectAttributes) {
-    // int statusEditMonHoc = monHocService.editMonHoc(mamh, tenmh, soTietLt,
-    // soTietTh);
-    // redirectAttributes.addFlashAttribute("statusThemMonHoc", statusEditMonHoc);
-    // return "redirect:/admin/subject";
-    // }
 
     @PostMapping("/editMonHoc")
     public String editMonHoc(@ModelAttribute("monHoc") MonHoc monHoc, RedirectAttributes redirectAttributes,
@@ -134,16 +110,17 @@ public class AdminController {
         }
         if (errors.hasErrors()) {
             for (FieldError error : errors.getFieldErrors()) {
-                errorMessage = error.getDefaultMessage();
-                break; // Chỉ lấy lỗi đầu tiên
+                redirectAttributes.addFlashAttribute("errorEdit", error.getDefaultMessage());
+                return "redirect:/admin/subject";
+                // redirectAttributes.addFlashAttribute("errorEdit_" + error.getField(),
+                // error.getDefaultMessage());
             }
-            redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
-            return "redirect:/admin/subject";
+
         }
 
         int statusEditMonHoc = monHocService.editMonHoc(monHoc.getMamh(), monHoc.getTenmh(), monHoc.getSoTietLt(),
                 monHoc.getSoTietTh());
-        redirectAttributes.addFlashAttribute("statusThemMonHoc", statusEditMonHoc);
+        redirectAttributes.addFlashAttribute("statusEditMonHoc", statusEditMonHoc);
         return "redirect:/admin/subject";
     }
 
@@ -152,6 +129,103 @@ public class AdminController {
         List<MonHoc> monHocList = monHocService.searchMonHoc(keyword);
         model.addAttribute("monHocList", monHocList);
         return "/admin/subject";
+    }
+
+    // =============================Giang Vien=============================
+    @ModelAttribute("giangVien")
+    public GiangVien createNewGiangVien() {
+        return new GiangVien();
+    }
+
+    @GetMapping("/lecturer")
+    public String lecturer(ModelMap model) {
+        List<GiangVien> giangVienList = giangVienService.findAllGiangVien();
+        model.addAttribute("giangVienList", giangVienList);
+        return "admin/lecturer";
+    }
+
+    @GetMapping("/deleteGiangVien/{maGv}")
+    public String getMethodName(@PathVariable("maGv") String maGv, RedirectAttributes redirectAttributes) {
+        int statusXoaGiangVien = giangVienService.deleteByMaGv(maGv);
+        redirectAttributes.addFlashAttribute("statusXoaGiangVien", statusXoaGiangVien);
+        return "redirect:/admin/lecturer";
+    }
+
+    @PostMapping("/addGiangVien")
+    public String addGiangVien(@ModelAttribute("giangVien") GiangVien giangVien, RedirectAttributes redirectAttributes,
+            BindingResult errors) {
+        if (giangVien.getHo().trim().length() == 0) {
+            errors.rejectValue("ho", "giangVien", "Họ giảng viên không được bỏ trống");
+        }
+        if (giangVien.getMaGv().trim().length() == 0) {
+            errors.rejectValue("maGv", "giangVien", "Mã giảng viên không được bỏ trống");
+        }
+        if (giangVien.getPassWord().trim().length() == 0) {
+            errors.rejectValue("passWord", "giangVien", "Password không được bỏ trống");
+        }
+        if (giangVien.getTen().trim().length() == 0) {
+            errors.rejectValue("ten", "giangVien", "Tên giảng viên không được bỏ trống");
+        }
+        if (giangVien.getUserName().trim().length() == 0) {
+            errors.rejectValue("userName", "giangVien", "Username không được bỏ trống");
+        }
+        if (giangVien.getEmail().trim().length() == 0) {
+            errors.rejectValue("email", "giangVien", "Email không được bỏ trống");
+        }
+        if (errors.hasErrors()) {
+            redirectAttributes.addFlashAttribute("errorAdd", true);
+            for (FieldError error : errors.getFieldErrors()) {
+                redirectAttributes.addFlashAttribute("errorAdd_" + error.getField(), error.getDefaultMessage());
+            }
+            return "redirect:/admin/lecturer";
+        }
+
+        giangVien.setTrangThaiXoa(false);
+        int statusThemGiangVien = giangVienService.addGiangVien(giangVien);
+        System.out.println("================================================");
+        System.out.println(statusThemGiangVien);
+        redirectAttributes.addFlashAttribute("statusThemGiangVien", statusThemGiangVien);
+        return "redirect:/admin/lecturer";
+    }
+
+    @PostMapping("/editGiangVien")
+    public String editGiangVien(@ModelAttribute("giangVien") GiangVien giangVien, RedirectAttributes redirectAttributes,
+            BindingResult errors) {
+        if (giangVien.getHo().trim().length() == 0) {
+            errors.rejectValue("ho", "giangVien", "Họ giảng viên không được bỏ trống");
+        }
+        if (giangVien.getMaGv().trim().length() == 0) {
+            errors.rejectValue("maGv", "giangVien", "Mã giảng viên không được bỏ trống");
+        }
+        if (giangVien.getTen().trim().length() == 0) {
+            errors.rejectValue("ten", "giangVien", "Tên giảng viên không được bỏ trống");
+        }
+        if (giangVien.getEmail().trim().length() == 0) {
+            errors.rejectValue("email", "giangVien", "Email không được bỏ trống");
+        }
+        if (errors.hasErrors()) {
+            for (FieldError error : errors.getFieldErrors()) {
+                redirectAttributes.addFlashAttribute("errorEdit", error.getDefaultMessage());
+                return "redirect:/admin/lecturer";
+                // redirectAttributes.addFlashAttribute("errorEdit_" + error.getField(),
+                // error.getDefaultMessage());
+            }
+            return "redirect:/admin/lecturer";
+        }
+
+        giangVien.setTrangThaiXoa(false);
+        int statusEditGiangVien = giangVienService.editGiangVien(giangVien.getHo(), giangVien.getTen(),
+                giangVien.getEmail(), giangVien.getHocVi(), giangVien.getHocHam(), giangVien.isGioiTinh(),
+                giangVien.getMaGv());
+        redirectAttributes.addFlashAttribute("statusEditGiangVien", statusEditGiangVien);
+        return "redirect:/admin/lecturer";
+    }
+
+    @GetMapping("/searchGiangVien")
+    public String searchGiangVien(@RequestParam(name = "keyword") String keyword, ModelMap model) {
+        List<GiangVien> giangVienList = giangVienService.searchGiangVien(keyword);
+        model.addAttribute("giangVienList", giangVienList);
+        return "admin/lecturer";
     }
 
 }
