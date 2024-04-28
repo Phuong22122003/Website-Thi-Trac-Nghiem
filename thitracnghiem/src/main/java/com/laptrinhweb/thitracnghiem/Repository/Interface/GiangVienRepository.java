76414f1eace42737debe360b9fe2d67ab73acaf9
nhoczui3110 <tranvietquang3110@gmail.com>
2024-04-28 20:13:42 +0700

    Add function (findAllGiangVien, findByMaGv, findByUserName, searchGiangVien)

--- thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Repository/Interface/GiangVienRepository.java
+++ thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Repository/Interface/GiangVienRepository.java
@@ -8,19 +8,31 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.laptrinhweb.thitracnghiem.Entity.GiangVien;
-
-
+import com.laptrinhweb.thitracnghiem.Entity.MonHoc;
 
 @Repository
 public interface GiangVienRepository extends JpaRepository<GiangVien, String> {
-    // @Query(value = "EXEC findUserByUsername @username = :username, @type = 'GIANGVIEN'", nativeQuery = true)
+    // @Query(value = "EXEC findUserByUsername @username = :username, @type =
+    // 'GIANGVIEN'", nativeQuery = true)
     // GiangVien findUserByUserName(@Param("username") String username);
 
-    // @Query(value = "select * from GIANGVIEN where GIANGVIEN.trangthaixoa=0", nativeQuery = true)
+    // @Query(value = "select * from GIANGVIEN where GIANGVIEN.trangthaixoa=0",
+    // nativeQuery = true)
     // public List<GiangVien> findAllGvWithoutDelete();
 
-    // @Query(value = "EXEC searchGiangVien @keyword = :keyword", nativeQuery = true)
+    // @Query(value = "EXEC searchGiangVien @keyword = :keyword", nativeQuery =
+    // true)
     // public List<GiangVien> searchGiangVien(@Param("keyword") String keyword);
 
     // public GiangVien findByUserName(String userName);
+
+    @Query(value = "select * from giangvien where trangthaixoa=0", nativeQuery = true)
+    public List<GiangVien> findAllGiangVien();
+
+    public GiangVien findByMaGv(String maGv);
+
+    public GiangVien findByUserName(String userName);
+
+    @Query(value = "exec searchGiangVien @keyword = :keyword", nativeQuery = true)
+    public List<GiangVien> searchGiangVien(@Param("keyword") String keyword);
 }
