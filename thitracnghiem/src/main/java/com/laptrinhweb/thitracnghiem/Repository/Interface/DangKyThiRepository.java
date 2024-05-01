13a7c75c27105c8565fa9fbb39e7136480658783
nhoczui3110 <tranvietquang3110@gmail.com>
2024-05-01 20:45:06 +0700

    Add findDangKyThiByMagv and searchDangKyThi

--- thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Repository/Interface/DangKyThiRepository.java
+++ thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Repository/Interface/DangKyThiRepository.java
@@ -3,8 +3,11 @@ package com.laptrinhweb.thitracnghiem.Repository.Interface;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.laptrinhweb.thitracnghiem.DTO.DangKyThiDTO;
 import com.laptrinhweb.thitracnghiem.Entity.DangKyThi;
 import com.laptrinhweb.thitracnghiem.Entity.Lop;
 import com.laptrinhweb.thitracnghiem.Entity.MonHoc;
@@ -25,5 +28,11 @@ public interface DangKyThiRepository extends JpaRepository<DangKyThi, Integer> {
 
     // long countByMamhAndMagvAndTrangThaiXoa(String mamh, String magv, boolean
     // trangThaiXoa);
+    // @Query(value = "EXEC FINDDANGKYTHIBYMAGV @MAGV = ?1", nativeQuery = true)
+    // List<DangKyThiDTO> findByMaGv(String magv);
+    @Query(value = "EXEC FINDDANGKYTHIBYMAGV :magv", nativeQuery = true)
+    List<DangKyThiDTO> findDangKyThiByMagv(@Param("magv") String magv);
 
+    @Query(value = "EXEC searchDangKyThi :keyword, :MAGV", nativeQuery = true)
+    List<DangKyThiDTO> searchDangKyThi(@Param("keyword") String keyword, @Param("MAGV") String maGv);
 }
