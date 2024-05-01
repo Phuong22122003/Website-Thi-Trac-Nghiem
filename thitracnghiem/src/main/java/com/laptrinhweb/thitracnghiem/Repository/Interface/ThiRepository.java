2ba0e468879d08fbd39c5f14017d2432eef75aaa
nhoczui3110 <tranvietquang3110@gmail.com>
2024-05-01 20:45:34 +0700

    add findThiFromIddk and insertThiByIddk

--- thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Repository/Interface/ThiRepository.java
+++ thitracnghiem/src/main/java/com/laptrinhweb/thitracnghiem/Repository/Interface/ThiRepository.java
@@ -1,16 +1,18 @@
 package com.laptrinhweb.thitracnghiem.Repository.Interface;
 
-
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.laptrinhweb.thitracnghiem.DTO.DangKyThiDTO;
+import com.laptrinhweb.thitracnghiem.DTO.ThiDTO;
 import com.laptrinhweb.thitracnghiem.DTO.ThiInfoDTO;
 import com.laptrinhweb.thitracnghiem.Entity.Thi;
-
 
 @Repository
 public interface ThiRepository extends JpaRepository<Thi, Integer> {
@@ -20,11 +22,18 @@ public interface ThiRepository extends JpaRepository<Thi, Integer> {
     // @Query(value = "EXEC insertThiByIddk @iddk = :iddk", nativeQuery = true)
     // public void insertThiByIddk(@Param("iddk") int iddk);
 
-    // @Query(value = "SELECT * FROM Thi t where t.masv = :masv and t.trangThaiXoa = 0", nativeQuery = true)
+    // @Query(value = "SELECT * FROM Thi t where t.masv = :masv and t.trangThaiXoa =
+    // 0", nativeQuery = true)
     // public List<Thi> checkExsitsThi(@Param("masv") String masv);
 
     @Transactional
     @Modifying
     @Query("update Thi t set t.dathi = true,t.diem = :diem where t.idThi = :idthi")
     public void update(@Param(value = "idthi") int idthi, @Param(value = "diem") float diem);
+
+    @Query(value = "EXEC findThiFromIddk :iddk", nativeQuery = true)
+    List<ThiDTO> findThiFromIddk(@Param("iddk") int iddk);
+
+    @Procedure
+    void insertThiByIddk(@Param("iddk") int iddk);
 }
